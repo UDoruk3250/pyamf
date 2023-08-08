2fb62b1832a38ceb9016c42c121cf57fcc2fe21c
Doruk Alp <dorukalpuzunarslan@gmail.com>
2023-08-08 10:10:28 +0300

    Update Builder2D.py

--- amfpy/Builder2D.py
+++ amfpy/Builder2D.py
@@ -1,10 +1,11 @@
 import matplotlib.pyplot as plt
+from constants import *
 
 
 class Drawer:
-    def plotAtom(self, x: float, y: float, atomnumber: int):
-        plt.plot(x,y)
 
+    def plotAtom(self, x: float, y: float, atomnumber: int):
+        plt.plot(x, y, marker="o", color=getAtomColor(atomnumber))
     def plotBond(self):
         pass
 
