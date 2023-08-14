a0627330ad7f6d854627a3e5dde1946cea537d99
Doruk Alp <dorukalpuzunarslan@gmail.com>
2023-08-14 20:35:40 +0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.1.2'
+DESCRIPTION = 'Absolute Molecule File library extension for python'
+LONG_DESCRIPTION = 'Absolute Molecule File library extension for python'
+
+setup(name='amfpy-python',
+      version=VERSION,
+      author='Doruk Alp Uzunarslan',
+      author_email='duzunarslan27@my.uaa.k12.tr',
+      description=DESCRIPTION,
+      long_description=LONG_DESCRIPTION,
+      packages=find_packages(),
+      install_requires=['matplotlib'],
+      keywords=['python', 'modelling', 'molecule', 'atom', 'molecular modelling'])
