805b84e4d36b7dd96c25ffa403e7f1d74651a7b6
Doruk Alp <dorukalpuzunarslan@gmail.com>
2023-08-16 20:30:41 +0300

    Update BondBuilder.py

--- amfpy/BondBuilder.py
+++ amfpy/BondBuilder.py
@@ -8,20 +8,10 @@ class BondBuilder:
 
     def singleBond(self, atomList: list, bondList: list):
         self.drawer.plotBond(
-            [float(atomList[int(bondList[-1][3]) - 1][3]), float(atomList[int(bondList[-1][4]) - 1][3])],
-            [float(atomList[int(bondList[-1][3]) - 1][4]), float(atomList[int(bondList[-1][4]) - 1][4])])
+           [float(atomList[int(bondList[-1][3]) - 1][3]), float(atomList[int(bondList[-1][4]) - 1][3])],
+           [float(atomList[int(bondList[-1][3]) - 1][4]), float(atomList[int(bondList[-1][4]) - 1][4])])
 
     def doubleBond(self, atomList: list, bondList: list):
-        # float result = ( Mathf.Atan((mousePos.x - startAtom.transform.position.x) / ((mousePos.y -
-        # startAtom.transform.position.y))));
-        #
-        # line1.SetPosition(0, startAtom.transform.position + new
-        # Vector3(distance * Mathf.Cos(result), distance * Mathf.Sin(result + (45 * Mathf.PI)), 1
-        # f));
-        # line2.SetPosition(0, startAtom.transform.position + new
-        # Vector3(distance * Mathf.Cos(result + (45 * Mathf.PI)), distance * Mathf.Sin(result), 1
-        # f));
-
         result = m.atan(
             ((float(atomList[int(bondList[-1][4]) - 1][3])) - (float(atomList[int(bondList[-1][3]) - 1][3]))) / (
                     (float(atomList[int(bondList[-1][4]) - 1][4])) - (float(atomList[int(bondList[-1][3]) - 1][4]))))
@@ -39,9 +29,9 @@ class BondBuilder:
         #          color='dimgrey', linewidth=2)
 
         self.drawer.plotBond([float(atomList[int(bondList[-1][3]) - 1][3]) + (distance * m.cos(result + (45 * m.pi))),
-                  float(atomList[int(bondList[-1][4]) - 1][3]) + (distance * m.cos(result + (45 * m.pi)))],
-                 [float(atomList[int(bondList[-1][3]) - 1][4]) + (distance * m.sin(result)),
-                  float(atomList[int(bondList[-1][4]) - 1][4]) + (distance * m.sin(result))])
+                              float(atomList[int(bondList[-1][4]) - 1][3]) + (distance * m.cos(result + (45 * m.pi)))],
+                             [float(atomList[int(bondList[-1][3]) - 1][4]) + (distance * m.sin(result)),
+                              float(atomList[int(bondList[-1][4]) - 1][4]) + (distance * m.sin(result))])
 
     def tripleBond(self, atomList: list, bondList: list):
         pass
