acb6870732a2f1c77c87de55f3380cfded10d2bd
Doruk Alp <dorukalpuzunarslan@gmail.com>
2023-08-28 17:29:50 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.1.2'
+VERSION = '0.1.5'
 DESCRIPTION = 'Absolute Molecule File library extension for python'
-LONG_DESCRIPTION = 'Absolute Molecule File library extension for python. Absolute Molecule File (*.amf) is a ' \
-                   'specific kind of a filetype that allows people to create molecules and proteins easily.'
+LONG_DESCRIPTION = 'Absolute Molecule File library extension for python. Absolute Molecule File (*.amf) is a specific kind of a filetype that allows people to create molecules and proteins easily. You can access the documentation from [here](https://UDoruk3250.github.io/amf/Documentation)'
+
 
 setup(name='amfpy-python',
       version=VERSION,
@@ -11,6 +11,7 @@ setup(name='amfpy-python',
       author_email='duzunarslan27@my.uaa.k12.tr',
       description=DESCRIPTION,
       long_description=LONG_DESCRIPTION,
+      long_description_content_type="text/markdown",
       packages=find_packages(),
       install_requires=['matplotlib'],
-      keywords=['python', 'modelling', 'molecule', 'atom', 'molecular modelling'])
+      keywords=['python', 'modelling', 'molecule', 'atom', 'molecular modelling', 'bioinformatics'])
