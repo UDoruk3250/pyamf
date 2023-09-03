130c17d37dc25bfe8dde34f115e444230453edca
Doruk Alp <dorukalpuzunarslan@gmail.com>
2023-09-03 20:21:11 +0300

    Update __init__.py

--- amfpy/__init__.py
+++ amfpy/__init__.py
@@ -1,13 +1,12 @@
 from .amfreader import *
 from .Builder2D import *
 from .Animator2D import *
+from time import sleep
 from multiprocessing import Process
 
 
 def build():
     Drawer.plotAll()
-    while True:
-        pass
 
 def animate(f: int):
     print("HEY!!")
