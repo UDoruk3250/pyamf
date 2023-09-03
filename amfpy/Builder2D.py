c76c07a6a2dc1d3c9be3a364991db36d87433dc4
Doruk Alp <dorukalpuzunarslan@gmail.com>
2023-09-03 20:21:14 +0300

    Update Builder2D.py

--- amfpy/Builder2D.py
+++ amfpy/Builder2D.py
@@ -1,10 +1,12 @@
 import matplotlib.pyplot as plt
+import matplotlib
 from .constants import *
 
 
 class Drawer:
     def __init__(self):
         plt.ion()
+
     def plotAtom(self, x: float, y: float, atomnumber: int):
         plt.plot(x, y, marker="o", color=getAtomColor(atomnumber), zorder=2)
 
@@ -13,4 +15,4 @@ class Drawer:
 
     @staticmethod
     def plotAll():
-        plt.show()
+        plt.show(block=True)
