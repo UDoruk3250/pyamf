18a8064be8d3a3b79e020ae174331fbaa8ae58d6
Doruk Alp <dorukalpuzunarslan@gmail.com>
2023-09-09 12:05:55 +0300

    Update Animator2D.py

--- amfpy/Animator2D.py
+++ amfpy/Animator2D.py
@@ -3,10 +3,12 @@ from .amfreader import *
 
 bondLength = []
 
+CorruptedFileError = Exception("The *.amf file has been corrupted or formatted wrong. Check the file again.")
+
 
 def animate2D(f: int):
     # for i in range(f):
     atomlist, bondlist = Reader.getLists()
-    if len(atomlist) != len(bondlist):
-        yield
+    if len(atomlist) != len(bondlist)+1:
+        raise CorruptedFileError
         # print("Step " + str(i))
