9f113daf6805caa799c07f1b3ae69eb345bec60d
Doruk Alp <dorukalpuzunarslan@gmail.com>
2023-09-09 12:42:16 +0300

    Update Animator2D.py

--- amfpy/Animator2D.py
+++ amfpy/Animator2D.py
@@ -6,11 +6,24 @@ bondLength = []
 CorruptedFileError = Exception("The *.amf file has been corrupted or formatted wrong. Check the file again.")
 
 
+class Atom:
+    def __init__(self, thelist):  # TODO: add atom number as well.
+        self.id = thelist[1]
+        self.atomnumber = thelist[2]
+        self.x = thelist[3]
+        self.y = thelist[4]
+
+
 def animate2D(f: int):
-    # for i in range(f):
-    atomlist, bondlist = Reader.getLists()
-    if len(atomlist) != len(bondlist):
+    atomlists, bondlists = Reader.getLists()
+    if len(atomlists) != len(bondlists):
         raise CorruptedFileError
-    print("a")
+    for atomlist, bondlist in zip(atomlists, bondlists):
+        for i in range(f // 60):
+            for bond in bondlist:
+                atom1 = Atom(atomlist[int(bond[3]) - 1])
+                atom2 = Atom(atomlist[int(bond[4]) - 1])
+                distance = calculateDistance(atom1.x, atom2.x, atom1.y, atom2.y)
+
 
     # print("Step " + str(i))
