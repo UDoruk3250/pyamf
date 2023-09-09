71414f3f57fbdd4e9857f804add3ecc53490c718
Doruk Alp <dorukalpuzunarslan@gmail.com>
2023-09-09 16:07:01 +0300

    Update Builder2D.py

--- amfpy/Builder2D.py
+++ amfpy/Builder2D.py
@@ -6,6 +6,8 @@ class Drawer:
     def __init__(self):
         plt.ion()
 
+    def clear(self):
+        plt.clf()
     def plotAtom(self, x: float, y: float, atomnumber: int):
         plt.plot(x, y, marker="o", color=getAtomColor(atomnumber), zorder=2)
 
@@ -13,5 +15,15 @@ class Drawer:
         plt.plot(xlist, ylist, color="dimgrey", linewidth=2, zorder=1)
 
     @staticmethod
-    def plotAll():
-        plt.show(block=True)
+    def buildFromLists(atomlist, bondlist):
+        for atom in atomlist:
+            plt.plot(int(atom[3]), int(atom[4]), getAtomColor(int(atom[2])))
+        for bond in bondlist:
+            Drawer.plotBond(
+                [float(atomlist[int(bond[3]) - 1][3]), float(atomlist[int(bond[4]) - 1][3])],
+                [float(atomlist[int(bond[3]) - 1][4]), float(atomlist[int(bond[4]) - 1][4])]
+            )
+
+    @staticmethod
+    def plotAll(block: bool):
+        plt.show(block=block)
