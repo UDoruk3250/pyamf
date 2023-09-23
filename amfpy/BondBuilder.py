4c3bb82d44490fa26c5434cb0b0f8a866db905d6
Doruk Alp <dorukalpuzunarslan@gmail.com>
2023-09-23 17:48:48 +0300

    Update BondBuilder.py

--- amfpy/BondBuilder.py
+++ amfpy/BondBuilder.py
@@ -30,7 +30,10 @@ class BondBuilder:
         result = m.atan(
             ((float(atomlist[int(bondlist[-1][4]) - 1][3])) - (float(atomlist[int(bondlist[-1][3]) - 1][3]))) / (
                     (float(atomlist[int(bondlist[-1][4]) - 1][4])) - (float(atomlist[int(bondlist[-1][3]) - 1][4]))))
-        distance = 0.1
+        distance = 0.15
+        self.drawer.plotBond(
+            [float(atomlist[int(bondlist[-1][3]) - 1][3]), float(atomlist[int(bondlist[-1][4]) - 1][3])],
+            [float(atomlist[int(bondlist[-1][3]) - 1][4]), float(atomlist[int(bondlist[-1][4]) - 1][4])])
         self.drawer.plotBond([float(atomlist[int(bondlist[-1][3]) - 1][3]) + (distance * m.cos(result)),
                               float(atomlist[int(bondlist[-1][4]) - 1][3]) + (distance * m.cos(result))],
                              [float(atomlist[int(bondlist[-1][3]) - 1][4]) + (distance * m.sin(result + (45 * m.pi))),
